bfca13d98259cfc0eaaa6ecd16079d5aa86a98e7
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2021-10-16 10:54:27 -0400

    added support for numbers greater than 9

--- Stack.h
+++ Stack.h
@@ -2,17 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* this is my low IQ solution for distinguishing numbers from operators since everything is represented as an integer */
+
+typedef enum
+{
+    NONE, NUMBER, OPERATOR
+} expression;
+
+typedef struct 
+{
+    int term;
+    expression expr;
+} entry;
+
 typedef struct
 {
-    int* data;
+    entry* data;
     size_t size;    
     size_t capacity;
 } Stack;
 
+const char* exprToStr(expression e) 
+{
+    switch(e)
+    {
+        case NONE:     return "NONE";
+        case NUMBER:   return "NUMBER";
+        case OPERATOR: return "OPERATOR";
+    }
+}
+
 Stack* createStack(size_t cap)
 {
     Stack* s = (Stack*)malloc(sizeof(Stack));
-    s->data = (int*)malloc(sizeof(int) * cap);
+    s->data = (entry*)malloc(sizeof(entry) * cap);
     s->size = 0;
     s->capacity = cap;
     return s;
@@ -28,7 +51,7 @@ int isStackEmpty(Stack* s)
     return (s->size == 0);
 }
 
-int intcpy(int* dest, int* src, size_t size)
+int entrycpy(entry* dest, entry* src, size_t size)
 {
     if(!dest || !src)
         return 0;
@@ -42,18 +65,18 @@ int resizeStack(Stack* s)
     if(isStackFull(s))
     {
         s->capacity *= 2;
-        int* temp = (int*)malloc(sizeof(int) * s->size);
-        intcpy(temp, s->data, s->size);
+        entry* temp = (entry*)malloc(sizeof(entry) * s->size);
+        entrycpy(temp, s->data, s->size);
         free(s->data);
-        s->data = (int*)malloc(sizeof(int) * s->capacity);
-        intcpy(s->data, temp, s->size+1);
+        s->data = (entry*)malloc(sizeof(entry) * s->capacity);
+        entrycpy(s->data, temp, s->size+1);
         free(temp);
         return 1;
     }
     return 0;
 }
 
-int push(Stack* stack, int data)
+int push(Stack* stack, entry data)
 {      
     if(isStackFull(stack))
         resizeStack(stack);
@@ -62,14 +85,15 @@ int push(Stack* stack, int data)
     return 1;
 }
 
-int pop(Stack* stack)
+entry pop(Stack* stack)
 {
     if(!isStackEmpty(stack))
         return stack->data[--stack->size];
-    return __INT_MAX__;
+    entry e = {__INT_MAX__, NONE};
+    return e;
 }
 
-int top(Stack* s)
+entry top(Stack* s)
 {
     return s->data[s->size-1];
 }
@@ -79,7 +103,7 @@ void printStack(Stack* s)
     printf("size: %lu\n", s->size);
     printf("capacity: %lu \n", s->capacity);
     for(int i = 0; i < s->size; i++)
-        printf("%i ", s->data[i]);    
+        printf("(%i %s)\n", s->data[i].term, exprToStr(s->data[i].expr));    
     printf("\n");
 }
 
@@ -93,6 +117,6 @@ char* stackString(Stack* s)
 {
     char* stackString = (char*)malloc(sizeof(char) * s->size);
     for(int i = 0; i < s->size; i++)
-        stackString[i] = s->data[i];
+        stackString[i] = s->data[i].term;
     return stackString;
 }
--- main.c
+++ main.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include <math.h>
-#include "Stack.h"
 #include <ctype.h>
+
+#include "Stack.h"
 
 typedef enum 
 {
     LEFT_TO_RIGHT, RIGHT_TO_LEFT
 } Associativity;
+
 
 int isOperator(char c)
 {
@@ -29,7 +30,7 @@ int operatorPrecedence(char operator)
     case '(': return -1;
     case ')': return -1;
     default:
-        printf("%s: %c\n", "Nonvalid Operator", operator);
+        printf("%s: %c\n", "Invalid Operator", operator);
     }
     return -1;
 }
@@ -43,6 +44,7 @@ Associativity operatorAssociativity(char operator)
         case '*': return LEFT_TO_RIGHT;
         case '/': return LEFT_TO_RIGHT;
         case '^': return RIGHT_TO_LEFT;
+        default:  return -1;
     }
 }
 
@@ -53,126 +55,152 @@ void printResults(char input, Stack* opStack, const char* pfExp)
     free(stackStr);
 }
 
-char* infixToPostfix(const char* infixExp)
+Stack* infixToPostfix(const char* infixExp)
 {
     Stack* opStack = createStack(5);
-    char* postfixExp = (char*)malloc(sizeof(char) * 64);
-    int postfixIndex = 0;    
+    Stack* postFixStack = createStack(10);
+    char num[16];
     for(int i = 0; i < strlen(infixExp); i++)
     {
+        int j = 0;
         char c = infixExp[i];
-        if(isalnum(c))
-            postfixExp[postfixIndex++] = c;
-        
-        else if(isOperator(c))            
+        while(isdigit(c))
         {
-            if(isStackEmpty(opStack) || operatorPrecedence(c) > operatorPrecedence(top(opStack)))
-                push(opStack, c);            
-            
+            num[j++] = c;
+            i++;
+            c = infixExp[i];
+        }
+        num[j] = '\0';
+        if(strlen(num))
+        {
+            //printf("num %s\n", num);
+            int n = atoi(num);
+            entry e = {n, NUMBER};
+            //printf("Pushing %d\n", n);
+            push(postFixStack, e);
+            //printStack(postFixStack);
+        }
+        if(isOperator(c))            
+        {
+            entry e = {c, OPERATOR};
+            if(isStackEmpty(opStack) || operatorPrecedence(c) > operatorPrecedence(top(opStack).term))
+                push(opStack, e);                                   
             else
             {
-                while(!isStackEmpty(opStack) && operatorPrecedence(c) <= operatorPrecedence(top(opStack)))
+                while(!isStackEmpty(opStack) && operatorPrecedence(c) <= operatorPrecedence(top(opStack).term))
                 {
-                    if(operatorPrecedence(c) < operatorPrecedence(top(opStack)))
-                        postfixExp[postfixIndex++] = pop(opStack);
+                    if(operatorPrecedence(c) < operatorPrecedence(top(opStack).term))
+                        push(postFixStack, pop(opStack));
                       
-                    else if (operatorPrecedence(c) == operatorPrecedence(top(opStack)))
+                    else if (operatorPrecedence(c) == operatorPrecedence(top(opStack).term))
                     {                                     
                         if(operatorAssociativity(c) == LEFT_TO_RIGHT)
-                            postfixExp[postfixIndex++] = pop(opStack);  
-
+                            push(postFixStack, pop(opStack));
                         else if(operatorAssociativity(c) == RIGHT_TO_LEFT)
                             break;                        
                     }
                 }
-                push(opStack, c);
+                push(opStack, e);
             }
         }   
         else if (c == '(' || c == ')')
         {
+            entry e = {c, OPERATOR};
             if(c == '(')
-                push(opStack, c);
+                push(opStack, e);
             else
             {
-                while(top(opStack) != '(')
-                    postfixExp[postfixIndex++] = pop(opStack);
+                while(top(opStack).term != '(')
+                    push(postFixStack, pop(opStack));
                 pop(opStack); // pop (
             }
         }      
     }
     while(!isStackEmpty(opStack))
-        postfixExp[postfixIndex++] = pop(opStack);
+        push(postFixStack, pop(opStack));
     
     deleteStack(opStack);
-    return postfixExp;
+    return postFixStack;
 }
 
-float evaluatePostfixExpression(const char* postfixExp)
+int evaluatePostfixExpression(Stack* postfixExp)
 {
     Stack* evalStack = createStack(5);
-    for(int i = 0; i < strlen(postfixExp); i++)
+    for(int i = 0; i < postfixExp->size; i++)
     {
-        char c = postfixExp[i];
-        if(isAlphaNumeric(c))
-            push(evalStack, c-'0');
-        else if (isOperator(c))
+        entry e = postfixExp->data[i];
+        //printf("c: %d\n", e.term);  
+        // here is how it distinguishes from numbers and operators
+        // for example the number 43 maps to + the program would get confused whether to use 43 as an operator or as a number      
+        if(e.expr == NUMBER)
         {
-            int op1 = pop(evalStack);
-            int op2 = pop(evalStack);            
-            switch(c)
+            //printf("pushing %d\n", e.term);
+            push(evalStack, e);
+        }
+        else if (e.expr == OPERATOR)
+        {
+            entry op1 = pop(evalStack);
+            entry op2 = pop(evalStack);    
+            switch(e.term)
             {
                 case '*': 
                 {
-                    int result = op1 * op2;
+                    int result = op1.term * op2.term;
                     //printf("%i %c %i = %i\n", op1, c, op2, result);
-                    push(evalStack, result);
+                    entry r = {result, NUMBER};
+                    push(evalStack, r);
                 }
                 break;
                 case '+':    
                 {
-                    int result = op1 + op2;
-                    //printf("%i %c %i = %i\n", op1, c, op2, result);
-                    push(evalStack, result);
+                    int result = op1.term + op2.term;
+                    //printf("%i %c %i = %i\n", op1, e.term, op2, result);
+                    entry r = {result, NUMBER};
+                    push(evalStack, r);
                 }                
                 break;
                 case '-':   
                 {
-                    int result = op2 - op1;
+                    int result = op2.term - op1.term;
                     //printf("%i %c %i = %i\n", op2, c, op1, result);
-                    push(evalStack, result);
+                    entry r = {result, NUMBER};
+                    push(evalStack, r);
                 }                 
                 break;
                 case '/':  
                 {
-                    int result = op2 / op1;
+                    int result = op2.term / op1.term;
                     //printf("%i %c %i = %i\n", op2, c, op1, result);
-                    push(evalStack, result);
+                    entry r = {result, NUMBER};
+                    push(evalStack, r);
                 }                  
                 break;
                 case '^': 
                 {
-                    int result = pow(op2, op1);
-                    printf("%i %c %i = %i\n", op2, c, op1, result);
-                    push(evalStack, result);
+                    int result = pow(op2.term, op1.term);
+                    //printf("%i %c %i = %i\n", op2, c, op1, result);
+                    entry r = {result, NUMBER};
+                    push(evalStack, r);
                 }                  
                 break;
                 default:
-                    printf("%s", "Non-valid operator");
+                    printf("%s", "Invalid Operator");
             }
         }
     }
-    int result = pop(evalStack);
+    entry result = pop(evalStack);
     deleteStack(evalStack);
-    return result;
+    return result.term;
 }
 
 int main(int argc, char** argv)
 {    
     char infixExpression[128];
     scanf("%s", infixExpression);
-    char* postfixExp = infixToPostfix(infixExpression);
-    int result = evaluatePostfixExpression(postfixExp);
+    Stack* postFixStack = infixToPostfix(infixExpression);
+    //printStack(postFixStack);
+    int result = evaluatePostfixExpression(postFixStack);
     printf("%i\n", result);
-    free(postfixExp);
+    free(postFixStack);
     return 0;
 }
